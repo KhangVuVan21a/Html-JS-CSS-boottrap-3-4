ee640f148d6664933470eaab9141671659eb3abc
KhangVuVan21a <khang21a@gmail.com>
2022-05-21 16:43:29 +0700

    form training

--- Html,js,CSS/js/main.js
+++ Html,js,CSS/js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 
 })
-$(".navbar a").click(function(){
+$(".navbar a").click(function () {
     $("body,html").animate({
-     scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-    
-   })
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+})
